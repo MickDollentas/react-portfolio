ca6df5b082c490538a0c5190e7044fd37f9ec617
Mickey <38341741+MickDollentas@users.noreply.github.com>
2022-07-20 23:09:43 -0700

    Portfolio section draft

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,19 +13,95 @@ const Experience = () => {
           <h3>Skills</h3>
           <div className="experience_content">
             <article className="experience_details">
-              <AiFillCheckCircle/>
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
               <h4>HTML</h4>
-              <small className='text-light'>Advanced</small>
+                <small className='text-light'>Expert</small>
+                </div>
             </article>
             <article className="experience_details">
-              <AiFillCheckCircle/>
-              <h4>CSS</h4>
-              <small className='text-light'>Advanced</small>
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Proficient</small>
+              </div>
             </article> 
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Competent</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Competent</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Beginner</small>
+              </div>
+            </article>
           </div>
         </div>
         <div className="experience_tools">
           <h3>Tools</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>Git</h4>
+                <small className='text-light'>Proficient</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>Text Editor</h4>
+                <small className='text-light'>Proficient</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>FTP Client</h4>
+                <small className='text-light'>Competent</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>GitHub</h4>
+                <small className='text-light'>Proficient</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>Photoshop</h4>
+                <small className='text-light'>Proficient</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>Figma</h4>
+                <small className='text-light'>Beginner</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <AiFillCheckCircle className='experience_details-icon'/>
+              <div>
+                <h4>CodeSandbox</h4>
+                <small className='text-light'>Proficient</small>
+              </div>
+            </article>
+          </div>
         </div>
       </div>
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/port2.png'
+import IMG2 from '../../assets/port4.png'
+import IMG3 from '../../assets/port3.png'
+import IMG4 from '../../assets/port1.png'
 
 function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio_container">
+        <article className='portfolio_item'>
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="Guess The Word"/>
+          </div>
+          <h3>Guess The Word</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com/MickDollentas/guess-the-word" className='btn' target='_blank'>Code</a>
+            <a href="https://mickdollentas.github.io/guess-the-word/" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+        <article className='portfolio_item'>
+          <div className="portfolio_item-image">
+            <img src={IMG2} alt="Unplugged Retreat"/>
+          </div>
+          <h3>Unplugged Retreat</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com/MickDollentas/unplugged-retreat" className='btn' target='_blank'>Code</a>
+            <a href="https://mickdollentas.github.io/unplugged-retreat/" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+        <article className='portfolio_item'>
+          <div className="portfolio_item-image">
+            <img src={IMG3} alt="Name Tag Generator"/>
+          </div>
+          <h3>Name Tag Generator</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://codesandbox.io/s/name-tag-generator-1632dv" className='btn' target='_blank'>Code</a>
+            <a href="https://1632dv.csb.app/" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+        <article className='portfolio_item'>
+          <div className="portfolio_item-image">
+            <img src={IMG4} alt="Sticky Notes"/>
+          </div>
+          <h3>Sticky Notes App</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://codesandbox.io/s/sticky-note-ui-wtck0r" className='btn' target='_blank'>Code</a>
+            <a href="https://wtck0r.csb.app/" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
+
 
 export default Portfolio
